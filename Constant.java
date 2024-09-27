d2abbfa7f6a7be202698bbab85b576cb7dccfd83
Suman Singh <148692093+suman1252@users.noreply.github.com>
2024-09-27 23:42:47 +0530

    Add files via upload

---
+++ Constant.java
@@ -0,0 +1,31 @@
+ /* Suman Kumari
+  * 27-09-2024
+  *  The class contains a collection of constant string values that are used throughout 
+  *  the application for user prompts, messages, and other fixed text.
+  */
+package NumberConversionAndCalculation;
+public class Constant {
+	public static String BASE = "\nBase Converter and Calculator";
+	public static String CHOOSE_OPTION = "Choose an option: ";
+	public static String CONVERT_BASE = "1. Convert between bases";
+	public static String PERFORM_OPERATION = "2. Perform arithmetic operations";
+	public static String EXIT = "3. Exit";
+	public static String ENTER_CHOICE = "Enter your choice: ";
+	public static String  EXITING_PROGRAM = "Exiting the program. Goodbye!";
+	public static String INVALID_CHOICE = "Invalid choice. Please choose again.";
+	public static String ENTER_NUMBER = "\nEnter the number, input base, and output base separated by spaces (e.g., 1010 2 10): ";
+	public static String INVALID_INPUT = "Invalid input format.";
+	public static String BASE_INPUT = "Base must be between 2 and 36."; 
+	public static String CONVERSION_FAILED = "Conversion failed.";
+	public static String CONVERTED_NUMBER = "Converted number: ";
+	public static String EXAMPLE = "For example: add 1010 2 1011 2";
+	public static String ENTER_OPERATION = "\nEnter the operation (add, subtract, multiply, divide), two numbers, their respective bases separated by spaces.";
+	public static String DECIMAL_CONVERSION_FAILED = "Conversion to decimal failed.";
+	public static String CANNOT_DIVIDE_ZERO = "Cannot divide by zero.";
+	public static String INVALID_OPERATION = "Invalid operation.";
+	public static String RESULT_IN_DECIMAL = "Result in decimal: ";
+	public static String CHOOSE_OUTPUT_BASE = "Choose output base:";
+	public static String BASE_BETWEEN_2_TO_36 ="Base must be between 2 and 36.";
+	public static String RESULT_IN_BASE = "Result in base ";
+	public static String INVALID_BASE_FORMAT = "Invalid format for base.";
+}
